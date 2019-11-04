7e4f8321f96e1b3ab9c1e6bbfe203c5f8a33781c
sandyGonzalez <sandygonzalez.612@gmail.com>
2019-11-04 10:55:44 -0600

    Agregando ejercicio de componentes de componentes, utilizando libreria fake para las imagenes, Creando el componente principal index y dos componentes mas AprovalCard Componente de aprovación con 2 botones y CommentDetail componente de un comentario

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail'; 
+import ApprovalCard from './ApprovalCard';
+
+const App = ()=>{
+
+    return(
+        <div className = " ui container comments">
+            <ApprovalCard>
+            <CommentDetail 
+                author = "Sam" 
+                timeAgo = "Today at 4:45PM" 
+                textComment = "Fantastic Blog!" 
+                avatar = {faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+            <CommentDetail 
+            author = "Alex"
+            timeAgo = "Today at 2:00AM" 
+            textComment = "Excelent Blog" 
+            avatar = {faker.image.avatar()}
+            />
+            </ApprovalCard>
+
+            <ApprovalCard>
+            <CommentDetail 
+                author = "Jane" 
+                timeAgo = "Yesterday at 5:00PM" 
+                textComment = "Bad Blog" 
+                avatar = {faker.image.avatar()}
+                />
+            </ApprovalCard>
+           
+           
+        </div>
+    );
+}
+
+ReactDOM.render(<App/>,document.querySelector('#root'));
